cd7857d276fd89adb813aa15924e13b2b9710c18
Helix <hissyu@hotmail.com>
2023-08-06 10:40:35 +0800

    游戏前界面基础（天赋选择、属性分配）

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -5,8 +5,8 @@ import { createStore } from "solid-js/store";
 import type { StoreNode, Store, SetStoreFunction } from "solid-js/store";
 
 import { createEventCandidates } from '../common/events-collection'
-import { CharacterStatus, createEmptyStatus } from '../common/CharacterStatus'
-import { GameContext } from '../common/game-context'
+import { CharacterBaseProperty, CharacterStatus, createEmptyStatus } from '../common/CharacterStatus'
+import { BeforeGameContext, StartedGameContext } from '../common/game-context'
 import { EventItem, EventChain, OptionItem, SingleEvent, weightedPickEvent, ConditionToken, StackableToken, filterReadyEvents, conditionsCheck } from '../common/events'
 
 import chance from 'chance'
@@ -53,9 +53,49 @@ const GamePage: Component = () => {
     type GameState = 'talent-choose' | 'property-upgrade' | 'game-start' | 'game-end';
     const [gameState, setGameState] = createSignal<GameState>('talent-choose');
 
-
+    // 玩家自由分配属性点数
+    const [propertyPoints, setPropertyPoints] = createSignal<number>(5);
     // 生成用于各种操作闭包的上下文参数
-    const makeGameContext = (eventThis?: EventItem): GameContext => ({
+    const makeBeforeGameContext = (): BeforeGameContext => {
+        const currentState = gameState();
+        if (currentState === 'talent-choose' || currentState === 'property-upgrade') {
+            return {
+                gameState: currentState,
+
+                setPropertyUpgradePointsModification(modification) {
+                    setPropertyPoints(p => p + modification);
+                },
+                setPropertyModification(modification) {
+                    Object.keys(modification).forEach(k => {
+                        const key = k as keyof CharacterStatus;
+                        modification[key] = modification[key]! + currentStatus()[key];
+                    })
+                    setCurrentStatus(s => ({
+                        ...s,
+                        ...modification
+                    }));
+                },
+                tokensToAdd(tokens) {
+                    tokens.forEach(nt => {
+                        insertToken(nt);
+                    })
+
+                },
+                pushGameState() {
+                    if (currentState === 'talent-choose') {
+                        setGameState('property-upgrade');
+                    }
+                    if (currentState === 'property-upgrade') {
+                        setGameState('game-start');
+                    }
+                },
+            }
+        }
+
+        throw new Error('game context not available');
+    }
+    const makeGameContext = (eventThis?: EventItem): StartedGameContext => ({
+        gameState: 'game-start',
         playerDetails: currentStatus(),
         reachedTokens: reachedTokens(),
         currentEvent: currentSingleEvent()!,
@@ -213,22 +253,38 @@ const GamePage: Component = () => {
         }
     }
 
+    const finishTalentChoose = (talents: Talent[]) => {
+        const ctx = makeBeforeGameContext();
+        talents.forEach(t => {
+            t.onApply(ctx)
+        })
+        ctx.pushGameState();
+    }
+    const finishPropertyUpgrade = (property: CharacterBaseProperty) => {
+        setCurrentStatus(s => ({ ...s, ...property }));
+        const ctx = makeBeforeGameContext();
+        ctx.pushGameState();
+    }
+
     onMount(() => {
         // TODO: 为每局游戏生成一个 seed， 作为全局的随机数发生器
         chanceInstance.seed = "Genenrate a seed";
-        const talents = createTalentCandidates(10, chanceInstance);
-        setTalentCadidate(talents);
+        const talents = createTalentCandidates(8, chanceInstance);
+        setTalentCadidates(talents);
         const events = createEventCandidates(20, chanceInstance);
         setAvailableEvents(events);
 
     })
 
-    const [talentCadidate, setTalentCadidate] = createSignal<Talent[]>([]);
+    const [talentCadidates, setTalentCadidates] = createSignal<Talent[]>([]);
 
     return <>
         <Switch>
             <Match when={gameState() === 'talent-choose'}>
-                <TelentChoosePage talents={talentCadidate()} talentPoints={3} />
+                <TelentChoosePage talents={talentCadidates()} talentPoints={3} finish={finishTalentChoose} />
+            </Match>
+            <Match when={gameState() === 'property-upgrade'}>
+                <PropertyUpgradePage availablePoints={propertyPoints()} baseProperty={currentStatus()} finish={finishPropertyUpgrade} />
             </Match>
             <Match when={gameState() === 'game-start'}>
                 <StatusBar playerCurrentStatus={currentStatus()} />
@@ -240,15 +296,91 @@ const GamePage: Component = () => {
 }
 export default GamePage;
 
+interface PropertyUpgradePageProps {
+    availablePoints: number;
+    baseProperty: CharacterBaseProperty;
+    finish: (property: CharacterBaseProperty) => void;
+}
+const PropertyUpgradePage: Component<PropertyUpgradePageProps> = (props) => {
+    const [baseProperty, setBaseProperty] = createSignal<CharacterBaseProperty>(props.baseProperty);
+    const [availablePoints, setAvailablePoints] = createSignal<number>(props.availablePoints);
+
+    const makePropertySetter = (property: keyof CharacterBaseProperty) => {
+        return (value: number) => {
+            if (value < 1 || value > 30) return;
+            if (value > baseProperty()[property]) {
+                if (availablePoints() <= 0) return;
+                setAvailablePoints(p => p - 1);
+            } else {
+                setAvailablePoints(p => p + 1);
+            }
+
+            setBaseProperty(p => ({ ...p, [property]: value }))
+        }
+    }
+    createEffect(() => {
+        setBaseProperty(props.baseProperty);
+    })
+    createEffect(() => {
+        setAvailablePoints(props.availablePoints);
+    })
+
+    return <>
+        <div class='flex flex-col items-center justify-center'>
+            <div class='text-3xl p-5'>属性提升 - 可用点数 {availablePoints()}</div>
+            <div class='flex flex-col items-center justify-center space-y-2'>
+                <UpgradePropertyItem name='力量' value={baseProperty().Constitution} setter={makePropertySetter('Constitution')} />
+                <UpgradePropertyItem name='灵巧' value={baseProperty().Dexterity} setter={makePropertySetter('Dexterity')} />
+                <UpgradePropertyItem name='智力' value={baseProperty().Intelligence} setter={makePropertySetter('Intelligence')} />
+                <UpgradePropertyItem name='意志' value={baseProperty().Willpower} setter={makePropertySetter('Willpower')} />
+                <UpgradePropertyItem name='直觉' value={baseProperty().Intuition} setter={makePropertySetter('Intuition')} />
+                <UpgradePropertyItem name='幸运' value={baseProperty().Luck} setter={makePropertySetter('Luck')} />
+            </div>
+            <button class='p-2 mt-5 rounded bg-blue-500 text-white' onclick={() => { props.finish(baseProperty()) }}>确认选择</button>
+        </div>
+    </>
+}
+
+interface UpgradePropertyItemProps {
+    name: string;
+    value: number;
+    setter: (v: number) => void;
+}
+const UpgradePropertyItem: Component<UpgradePropertyItemProps> = (props) =>
+(
+    <div class='flex flex-row space-x-2'>
+        <div class='text-xl'>{props.name}:&nbsp;</div>
+        <button class='text-xl border rounded w-7 h-7' onclick={() => { props.setter(props.value - 1) }}>
+            <div class={`${gameStyles.shortline} w-full h-full`} ></div>
+        </button>
+        <div class='text-xl'>{props.value}</div>
+        <button class='text-xl border rounded w-7 h-7' onclick={() => { props.setter(props.value + 1) }}>
+            <div class={`${gameStyles.crossline} w-full h-full`} ></div>
+        </button>
+    </div>
+)
+
 interface TalentChoosePageProps {
     talents: Talent[];
     talentPoints: number;
     // chosen: Talent[];
+    finish: (talents: Talent[]) => void;
 }
 const TelentChoosePage: Component<TalentChoosePageProps> = (props) => {
     const [chosen, setChosen] = createSignal<[Talent, boolean][]>(props.talents.map(t => [t, false]));
 
-    function handleChosen(p: { talent: Talent, index: number }) {
+    const [currentTalentPoints, setCurrentTalentPoints] = createSignal(props.talentPoints);
+
+    const [currentChosen, setCurrentChosen] = createSignal<Talent[]>([]);
+
+    createEffect(() => {
+        setChosen(() => props.talents.map(t => [t, false]))
+    });
+    createEffect(() => {
+        setCurrentChosen(() => chosen().filter(t => t[1]).map(t => t[0]));
+    })
+
+    function handleChoose(p: { talent: Talent, index: number }) {
         const newChosen: [Talent, boolean][] = chosen().map((t, i) => {
             if (i === p.index) {
                 return [t[0], !t[1]];
@@ -259,24 +391,26 @@ const TelentChoosePage: Component<TalentChoosePageProps> = (props) => {
         const prevCost = chosen().filter(t => t[1]).map(t => t[0].cost).reduce((a, b) => a + b, 0);
         const thisChosenCost = thisChosen[1] ? thisChosen[0].cost : -thisChosen[0].cost;
 
-        if (prevCost + thisChosenCost <= props.talentPoints)
+        if (prevCost + thisChosenCost <= props.talentPoints) {
             setChosen(newChosen);
+            setCurrentTalentPoints(props.talentPoints - prevCost - thisChosenCost);
+        }
+        else {
+            // TODO: 提示失败原因：超出点数
+        }
     }
 
     return <>
         <div class='flex flex-col items-center justify-center'>
-            <h1 class='text-3xl p-5'>选择天赋</h1>
+            <h1 class='text-3xl p-5'>选择天赋 - 可用点数: {currentTalentPoints()}</h1>
             <div class='flex flex-row flex-wrap space-x-5'>
                 <For each={chosen()}>
-                    {(talent, index) => <TalentItem talent={talent[0]} chosen={talent[1]} index={index()} onChosen={handleChosen} />}
+                    {(talent, index) => <TalentItem talent={talent[0]} chosen={talent[1]} index={index()} onChosen={handleChoose} />}
                 </For>
-                {/* <button class='flex flex-col items-center rounded border p-2 pt-1'>
-                    <span class='text-lg'>天赋1</span>
-                </button>
-                <button class={`flex flex-col items-center rounded border p-2 pt-1`}>
-                    <span class={`text-lg ${gameStyles.underline}`}>天赋2</span>
-                </button> */}
             </div>
+            <button class='p-2 mt-5 rounded bg-blue-500 text-white' onclick={() => {
+                props.finish(currentChosen());
+            }}>确认选择</button>
         </div >
     </>
 }
