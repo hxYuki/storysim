e08aa7ecdd6367a8e60e792bb476e1d286b4833f
Helix <hissyu@hotmail.com>
2023-08-06 10:40:58 +0800

    增加天赋实际功能

--- src/common/talents-collection.ts
+++ src/common/talents-collection.ts
@@ -5,10 +5,10 @@ export const TalentsCollection: Talent[] = [
     {
         id: 1,
         name: "天缺天予",
-        cost: -2,
+        cost: -1,
         description: "减少可分配属性，但可额外选择天赋",
-        apply(ctx) {
-
+        onApply(ctx) {
+            ctx.setPropertyUpgradePointsModification(-2);
         },
     },
     {
@@ -16,36 +16,45 @@ export const TalentsCollection: Talent[] = [
         name: "身强体健",
         cost: 1,
         description: "增加体质",
-        apply(ctx) { }
+        onApply(ctx) {
+            ctx.setPropertyModification({ Constitution: 3 });
+        }
     },
     {
         id: 3,
         name: "才气过人",
         cost: 1,
         description: "增加智力",
-        apply(ctx) { }
+        onApply(ctx) {
+            ctx.setPropertyModification({ Intelligence: 3 });
+        }
     },
     {
         id: 4,
         name: "快速反射",
         cost: 1,
         description: "你的反应速度快于常人，提升灵巧",
-        apply(ctx) { }
+        onApply(ctx) {
+            ctx.setPropertyModification({ Dexterity: 3 });
+        }
     },
     {
         id: 5,
         name: "通灵",
         cost: 1,
         description: "你与非物质世界有着不寻常的联系，灵感大幅提升。",
-        apply(ctx) { }
+        onApply(ctx) {
+            ctx.setPropertyModification({ Intuition: 3 });
+        }
     },
     {
         id: 6,
         name: "学姐的缎带",
         cost: 1,
         description: "一个温柔强大的学姐送给你的礼物，只是她后来不知为何失踪了。",
-        apply(ctx) {
-            // 必定会遭遇 魔法少女 事件
+        onApply(ctx) {
+            // TODO: 必定会遭遇 魔法少女 事件
+            ctx.tokensToAdd(["attracts-incubator"]);
         }
     },
 ];
