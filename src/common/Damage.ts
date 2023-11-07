9098f52b5a4a4de849a1ac9072286005f38feea9
Helix <hissyu@hotmail.com>
2023-11-07 21:20:19 +0800

    完善`伤害`结构

--- src/common/Damage.ts
+++ src/common/Damage.ts
@@ -1,17 +1,33 @@
-import { CharacterStatusProperty } from "./CharacterStatus";
+import { CharacterStatusCurentProperty, CharacterStatusProperty } from "./CharacterStatus";
 
 export class Damage {
-    raw: Partial<CharacterStatusProperty> = {};
+    raw: Partial<CharacterStatusCurentProperty> = {};
+
+
 
     // 伤害标记
     notKill = false;
 
-
-    // 数值取反
-    reverse() {
-        Object.keys(this.raw).forEach(k => {
+    public static fromRaw(raw: Partial<CharacterStatusProperty>) {
+        let damage = new Damage();
+        // map raw to current
+        Object.keys(raw).forEach(k => {
             const key = k as keyof CharacterStatusProperty;
+            if (raw[key])
+                damage.raw[StatusPropertyToCurrentProperty(key)] = raw[key]!;
+        })
+        // damage.raw = { HealthCurrent: raw.Health, SanityCurrent: raw.Sanity, StaminaCurrent: raw.Stamina };
+        return damage;
+    }
+    // 数值取反
+    public reverse() {
+        Object.keys(this.raw).forEach(k => {
+            const key = k as keyof CharacterStatusCurentProperty;
             this.raw[key] = -this.raw[key]!;
         })
     }
+}
+
+function StatusPropertyToCurrentProperty(status: keyof CharacterStatusProperty): keyof CharacterStatusCurentProperty {
+    return `${status}Current` as keyof CharacterStatusCurentProperty;
 }
